2cb60e6b37e27db420ec4163447afdcd42aecccb
fschmuck <florian.schmuck@bearingpoint.com>
2018-01-30 21:36:20 +0100

    [INFRASTRUCTURE] (HTTP) - added http methods

---
+++ src/app/books/book-service.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {BackendService} from "../shared/backend.service";
+import {Book} from "./model/book.model";
+
+@Injectable()
+export class BookService {
+
+  public books: Array<Book>;
+
+  constructor(private backendService: BackendService) {
+    this.backendService.get("books").subscribe((books) => {
+      this.books = books;
+    });
+  }
+
+  get allBooks(): Array<Book> {
+    return this.books;
+  }
+
+}
