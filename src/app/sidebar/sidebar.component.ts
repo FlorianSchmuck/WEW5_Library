49131d997dc6b09cfa5d28374ac1f93d8ebd418f
fschmuck <florian.schmuck@bearingpoint.com>
2018-02-01 07:07:24 +0100

    [Styling] (CSS) - fixed styling

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
 })
-export class SidebarComponent  {
-
+export class SidebarComponent {
 
 
 }
